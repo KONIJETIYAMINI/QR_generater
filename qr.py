e1e94359cc23d89d1757d3a4a556b8608b8f1406
KONIJETIYAMINI <konijetiyamini6@gmail.com>
2026-01-24 19:56:25 +0530

    Update qr.py

--- qr.py
+++ qr.py
@@ -1,19 +1,12 @@
 import qrcode
 
-qr_data = "https://https://github.com/KONIJETIYAMINI/capstone-qr/"
+url = "https://konijetiyamini.github.io/QR_generator/"
 
-qr = qrcode.QRCode(
-    version=3,
-    error_correction=qrcode.constants.ERROR_CORRECT_H,
-    box_size=10,
-    border=4,
-)
+qr = qrcode.make(url)
+qr.save("Capstone_Project_QR.png")
 
-qr.add_data(qr_data)
-qr.make(fit=True)
+print("QR Code ready")
 
-img = qr.make_image(fill_color="black", back_color="white")
-img.save("Capstone_UI_QR.png")
 
-print("QR Code generated successfully")
+
 
